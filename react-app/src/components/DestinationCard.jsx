7b8418bcfcb697bd0532c87142c1bb7697a8e526
Kalapos Dzsordzs Ulászló <harmathzs@gmail.com>
2025-10-20 08:35:09 +0200

    Finish DestinationCard

--- react-app/src/components/DestinationCard.jsx
+++ react-app/src/components/DestinationCard.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { Card, Form, Row, Col } from "react-bootstrap";
 
-export default function DestinationCard({city}) {
+export default function DestinationCard(props) {
+    // graceful props error handling:
+    let city = { image: '', name:'', country:'' }
+    if (props.city) city = props.city
+
     return (
         <>
             <Card className="mb-3 shadow-sm">
