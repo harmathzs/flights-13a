649bde666dae037955f2a86e2a9cbf822e2d96ee
Rafael Dzsúlió <harmathzs@gmail.com>
2025-10-20 08:57:22 +0200

    Create form in LoginForm

--- react-app/src/components/LoginForm.jsx
+++ react-app/src/components/LoginForm.jsx
@@ -1,7 +1,20 @@
+import React, { useState } from "react";
+import { Card, Form, Row, Col, Button } from "react-bootstrap";
+
 export default function LoginForm(props) {
     return (
         <>
-            <p>TODO - implement LoginForm component with any safe backend-powered auth options</p>
+            <Form>
+                <Form.Group className="mb-3" controlId="loginForm.email">
+                    <Form.Label>Email </Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="loginForm.password">
+                    <Form.Label>Password </Form.Label>
+                    <Form.Control type="password" />
+                </Form.Group>
+                <Button variant="primary">Login now</Button>
+            </Form>
         </>
     )
 }
